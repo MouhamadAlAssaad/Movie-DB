dd8ae229a23c4b0b39a6dc76da0b355571f58b3b
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-27 11:31:20 +0200

    step-4

--- index.js
+++ index.js
@@ -20,5 +20,25 @@ app.get("/time", (req,res) => {
 
 })
 
+app.get("/hello/ID", (req, res) => {
+    const ID = req.params.ID;
+
+    res.json({ status:200 , message:`hello,${ID}` })
+
+})
+
+app.get("/search", (req,res) => {
+
+    const search= req.query.search;
+    if (search){
+    res.json({ status:200 , message:'okk', data: search })}
+    
+    else{
+        res.json({ status: 500, error:true , mesage:"you have to provide a search"})
+    }
+
+
+
+})
 
 app.listen(3000, () => { console.log("server started on port 3000")})
