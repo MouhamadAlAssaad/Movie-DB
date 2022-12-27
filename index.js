ea6227cfd6260a6b704052fb55638630a3de857e
MouhamadAlAssaad <mhamad.said.2182000@gmail.com>
2022-12-27 10:57:40 +0200

    step-3

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const app = express()
 app.get("/", (req,res) => {
     res.send("ok")
 
+})
 
-
+app.get("/test", (req,res) => {
+    res.json({ status:200 , message:'okk' })
 
 })
+
+app.get("/time", (req,res) => {
+    const timenow = new Date()
+    const hours = timenow.getHours()
+    const mins = timenow.getMinutes()
+
+    res.json({ status:200 , message: `${hours}:${mins}` })
+
+})
+
+
 app.listen(3000, () => { console.log("server started on port 3000")})
